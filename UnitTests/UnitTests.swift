1f2b7100266040945b121a928dc101f70af86535
Fil <brej.filip@gmail.com>
2024-10-31 11:34:14 -0500

    add tests

--- UnitTests/UnitTests.swift
+++ UnitTests/UnitTests.swift
@@ -6,11 +6,32 @@
 //
 
 import Testing
+import Foundation
+@testable import fetch_recipe_app
 
 struct UnitTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test
+    func emptyState() async throws {
+        let viewModel = RecipeListViewModel()
+        viewModel.recipes = []
+        #expect(viewModel.recipes.count == 0)
     }
 
+    
+    @Test
+    func recipeParsing() {
+        let jsonData = """
+        {
+            "uuid": "1",
+            "name": "Pasta",
+            "cuisine": "Italian",
+        }
+        """.data(using: .utf8)!
+        
+        let recipe = try? JSONDecoder().decode(Recipe.self, from: jsonData)
+        #expect(recipe != nil)
+        #expect(recipe?.name == "Pasta")
+        #expect(recipe?.cuisine == "Italian")
+    }
 }
