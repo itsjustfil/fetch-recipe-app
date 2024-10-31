00635287a63ea33aa0364a1357bc16cf4fe4c27f
Fil <brej.filip@gmail.com>
2024-10-31 08:46:51 -0500

    Initial Commit

---
+++ fetch-recipe-app/fetch_recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_recipe_appApp.swift
+//  fetch-recipe-app
+//
+//  Created by Filip Brej on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
